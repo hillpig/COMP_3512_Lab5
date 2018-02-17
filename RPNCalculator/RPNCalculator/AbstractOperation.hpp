8a879ca0a9ece60ef9e37ab3dfefa33767f3382e
hillpig <hillpig@gmail.com>
2018-02-17 00:40:56 -0800

    AbstractOperation

---
+++ RPNCalculator/RPNCalculator/AbstractOperation.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+
+class AbstractOperation {
+
+private:
+	char operation_type;
+
+public:
+	AbstractOperation() = default;
+	AbstractOperation(char c) { operation_type = c; }
+	char get_code() const { return operation_type; }
+	virtual int ˜AbstractOperation() = 0;
+};
+
+inline int ˜AbstractOperation() {}
--- RPNCalculator/RPNCalculator/Operation.hpp
+++ RPNCalculator/RPNCalculator/Operation.hpp
@@ -1,11 +1,7 @@
 #pragma once
-#include <iostream>
-#include <string>
 
 class Operation {
 public:
-	char c;
-
 	virtual char get_code() const= 0;
 	virtual int perform(int, int) = 0;
 	virtual int ˜Operation() = 0;
